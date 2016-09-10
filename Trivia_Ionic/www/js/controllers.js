73a17fdc07e5ab7d49bb6df8bb2e346c79c65ef4
hdillon <dillonhoraciodavid@gmail.com>
2016-09-10 19:56:50 -0300

    Botones rojos o verdes según respuesta correcta/incorrecta del usuario

--- Trivia_Ionic/www/js/controllers.js
+++ Trivia_Ionic/www/js/controllers.js
@@ -8,7 +8,6 @@ angular.module('starter.controllers', [])
         $scope.showAlert("POR FAVOR INGRESE SU NOMBRE");
       else{
         $scope.showAlert("BIENVENIDO " + $scope.usuario.nombre + "!");
-        alert($scope.usuario.nombre);
         var usuario = { "nombre": $scope.usuario.nombre};
         $state.go('tab.jugar', usuario);
       }
@@ -25,15 +24,17 @@ angular.module('starter.controllers', [])
 })
 
 .controller('JugarCtrl', function($scope, $ionicPopup, $state, $stateParams, Preguntas, Respuestas, Opciones ) {
-  console.log($stateParams);
 
   $scope.nombreUsuario = angular.fromJson($stateParams);
   $scope.showComenzar = true;
   $scope.preguntas = Preguntas;
   $scope.respuestas = Respuestas;
   $scope.opciones = Opciones;
-  $scope.random = Math.round(Math.random() * 2); 
-  console.log($scope.random); 
+  $scope.btnOp1Estado = 'clear';
+  $scope.btnOp2Estado = 'clear';
+  $scope.btnOp3Estado = 'clear';
+
+  $scope.random = Math.round(Math.random() * 2); //TODO: Cambiar el random a un tanaño variable de a cuerdo a la cantidad de preguntas cargadas en firebase
 
   $scope.getPregunta = function() {
     if($scope.nombreUsuario.nombre == 'NOLOGUEADO'){
@@ -45,14 +46,35 @@ angular.module('starter.controllers', [])
   }
   };
 
-  $scope.setRespuesta = function(idOpcion, idPregunta, Respuesta) {
-    if(idOpcion == Respuesta)
+  $scope.setRespuesta = function(idOpcion, Respuesta, btnApretado) {
+    if(idOpcion == Respuesta){
+      $scope.cambiarColorBoton(btnApretado, 'correcto');
       $scope.showAlert("CORRECTO!");
-    else
+    }
+    else{
+      $scope.cambiarColorBoton(btnApretado, 'incorrecto');
       $scope.showAlert("INCORRECTO!");
+    }
     //recargo la variable random para que se recargue la pregunta
     $scope.random = Math.round(Math.random() * 2); 
   };
+
+
+  $scope.cambiarColorBoton = function(btnApretado, estado) {
+  switch(btnApretado){
+        case 'btnOp1':
+          $scope.btnOp1Estado = estado;
+        break;
+        case 'btnOp2':
+          $scope.btnOp2Estado = estado;
+        break;
+        case 'btnOp3':
+          $scope.btnOp3Estado = estado;
+        break;
+        default:
+        break;
+      }
+  }
 
   $scope.showAlert = function(resultado) {
   
